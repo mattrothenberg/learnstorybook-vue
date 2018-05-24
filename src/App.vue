7a393f4ad2fec4cb99b7a2d0f79f5f17114fb743
Matt Rothenberg <mrothenberg@obede.onmicrosoft.com>
2018-05-24 10:47:33 -0400

    feature: add styles from React example app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello
   </div>
 </template>
 
@@ -15,14 +14,3 @@ export default {
   }
 };
 </script>
-
-<style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
